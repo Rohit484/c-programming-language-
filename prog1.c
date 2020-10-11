bf9056cb9b6580c1518b774f8095aff5cde22462
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2020-10-11 14:43:47 +0530

    desktop/document

---
+++ prog1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,l,w;
+    printf("\n enter of length ");
+    scanf("%d",&l);
+    printf("\n enter of widht");
+    scanf("%d",&w);
+
+    a=l*w;
+    printf("\n your area is %d",a);
+}
