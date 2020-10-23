e30c1acae7963b31ba9cab00ac6e1fda1a6a76b9
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2020-10-23 22:44:18 +0530

    Add files via upload

---
+++ prog18.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int sum=0,no;
+    while(sum<100)
+    {
+
+        printf("\n your number is ");
+        scanf("%d",&no);
+        sum+=no;
+
+    }
+    printf("\n your sumis %d",sum);
+}
