b6f7eefd8986542a3639932d63a9a0215c121a24
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2020-11-04 11:51:47 +0530

    Add files via upload

---
+++ prog53.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int a[3],b[3],c[3],i;
+    printf("\n Array 1");
+    for(i=0;i<3;i++)
+    {
+        printf("\n Enter data ");
+        scanf("%d",&a[i]);
+    }
+    printf("\n Array 2");
+    for(i=0;i<3;i++)
+    {
+        printf("\n Enter data ");
+        scanf("%d",&b[i]);
+    }
+    for(i=0;i<3;i++)
+        c[i]=a[i]+b[i];
+        printf("\n Array 3");
+        for(i=0;i<3;i++)
+    printf("\n Your total array sum is %d +%d =%d",a[i],b[i],c[i]);
+
+
+
+}
