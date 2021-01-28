c6c3bc275f826f86aa7e0271ba9474e46720577d
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-01-28 21:01:46 +0530

    Add files via upload

---
+++ prog158.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int no,ad;
+    no=50;
+    printf("\n Your Minimum Balance is %d",no);
+    printf("\n Add your Balance in Basic Ammount in your Bank Account:- ");
+    scanf("%d",&ad);
+    no+=ad;
+    printf("\n Your now balance is %d",no);
+
+}
