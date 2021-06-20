31027e4925c21ca57f3266d2f812b2f57e58b7f3
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-06-20 22:26:54 +0530

    Add files via upload

---
+++ program24.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+    int i,n;
+    float s=0.0;
+    printf("\n Enter the number of terms: ");
+    scanf("%d",&n);
+    printf("\n\n");
+    for(i=1;i<=n;i++)
+    {
+        if(i<n)
+        {
+            printf("1%d + ",i);
+            s+=1/(float)i;
+
+        }
+        if(i==n)
+        {
+            printf("1/%d ",i);
+            s+=1/(float)i;
+        }
+    }
+    printf("\n Sum Of Series upto %d terms : %f \n",n,s);
+}
